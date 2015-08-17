9ab69d5bc69e5d1015efcfd40356b94eaf170c7e
Rich Kilmer <rich.kilmer@gmail.com>
2015-08-17 12:19:19 -0400

    update with more tests

--- test/test.js
+++ test/test.js
@@ -2,20 +2,26 @@ var USBDrive = require('../src/main.js');
 
 USBDrive.getAll()
   .then(function(usbDrives) {
-    console.log("GetAll:");
+    console.log("GetAll: "+usbDrives.length);
     console.log(usbDrives)
+    for(var i = 0;i<usbDrives.length;i++) {
+      USBDrive.get(usbDrives[i].id)
+        .then(function(usbDrive) {
+          if (usbDrive) {
+            console.log("Get: "+usbDrives[i].id);
+            console.log(usbDrive)
+            USBDrive.unmount(usbDrive.mount)
+              .then(function() {
+                console.log("Unmounted");
+              })
+              .catch(function() {
+                console.log("Not Unmounted");
+              });
+
+          } else {
+            console.log("Could not get "+usbDrives[i].id)
+          }
+        });
+    }
   });
 
-USBDrive.get("1234")
-  .then(function(usbDrive) {
-    console.log("Get");
-    console.log(usbDrive)
-  });
-
-USBDrive.unmount("/Volumes/Untitled")
-  .then(function() {
-    console.log("Unmounted");
-  })
-  .catch(function() {
-    console.log("Not Unmounted");
-  });
