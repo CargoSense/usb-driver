4f993660d3ef26fcf7e355fa7a41e5dd949472dd
Rich Kilmer <rich.kilmer@gmail.com>
2015-08-18 16:47:37 -0400

    break the tests up

---
+++ test/events.js
@@ -0,0 +1,23 @@
+var USBDrive = require('../src/main.js');
+
+USBDrive.on("attach", function(device) {
+  console.log("Attached!");
+  console.log(device);
+});
+
+USBDrive.on("detach", function(device) {
+  console.log("Detacehd!");
+  console.log(device);
+});
+
+USBDrive.on("mount", function(device) {
+  console.log("Mounted!");
+  console.log(device);
+});
+
+USBDrive.on("unmount", function(device) {
+  console.log("Unmounted!");
+  console.log(device);
+});
+
+USBDrive.waitForEvents();
--- test/test.js
+++ test/test.js
@@ -25,25 +25,3 @@ USBDrive.getAll()
         });
     }
   });
-
-USBDrive.on("attach", function(device) {
-  console.log("Attached!");
-  console.log(device);
-});
-
-USBDrive.on("detach", function(device) {
-  console.log("Detacehd!");
-  console.log(device);
-});
-
-USBDrive.on("mount", function(device) {
-  console.log("Mounted!");
-  console.log(device);
-});
-
-USBDrive.on("unmount", function(device) {
-  console.log("Unmounted!");
-  console.log(device);
-});
-
-USBDrive.waitForEvents();
